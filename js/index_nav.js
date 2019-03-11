0a3983a0d1cf609b4eb6519d82687ec7c4275f85
yc <786841292@qq.com>
2019-03-11 23:09:56 +0800

    02

---
+++ js/index_nav.js
@@ -0,0 +1,13 @@
+/*
+nav li event*/
+
+window.onload=function () {
+    let nav_div=document.getElementById('content');
+    let li_active=document.getElementById('li_active');
+    li_active.onfocus= function () {
+        let dd=nav_div.firstElementChild.firstElementChild.className;
+        nav_div.firstElementChild.firstElementChild.className='nav-item';
+        li_active.onfocus='';
+    };
+
+};
