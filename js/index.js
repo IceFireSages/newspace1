a60103ce228f726aa930818e7a0d8509d5b1dab4
IceFireSages <786841292@qq.com>
2019-04-28 21:02:18 +0800

    428

--- js/index.js
+++ js/index.js
@@ -1,39 +1,20 @@
 
 
 
-//portfolio
-/*$('document').ready(function () {
-  $('a[data-filter="*"]').click(function () {
-    $('div.portfolio-item').fadeIn()
-  });
-  $('[data-filter=".people"]').click(function () {
-    $('div.portfolio-items>div.people').siblings().fadeOut();
-    $('div.portfolio-items>div.people').css('display','block').fadeIn()
-  });
-  $('[data-filter=".wordpress"]').click(function () {
-    $('div.portfolio-items>div.wordpress').siblings().fadeOut();
-    $('div.portfolio-items>div.wordpress').css('display','block').fadeIn()
-  });
-  $('[data-filter=".joomla"]').click(function () {
-    $('div.portfolio-items>div.joomla').siblings().fadeOut();
-    $('div.portfolio-items>div.joomla').css('display','block').fadeIn()
-  });$('[data-filter=".megento"]').click(function () {
-    $('div.portfolio-items>div.megento').siblings().fadeOut();
-    $('div.portfolio-items>div.megento').css('display','block').fadeIn()
-  });
-  $('.single-blog').mouseover(function () {
-    $('.single-blog>h2').animate({color:"#fff"},800);
 
+
+$('document').ready(function () {
+  $('.single-service').focus(function () {
+    console.log('ddddddddd');
+    $('.single-service>i').css('backgroundColor','red')
   });
 
-});*/
-// portfolio filter
-$(window).load(function(){'use strict',
+  //portfolio
   $portfolio_selectors = $('.portfolio-filter >li>a');
   if($portfolio_selectors!='undefined'){
     $portfolio = $('.portfolio-items');
     $portfolio.isotope({
-      itemSelector : '.col-sm-3',
+      itemSelector : '.portfolio-item',
       layoutMode : 'fitRows'
     });
 
@@ -42,11 +23,8 @@ $(window).load(function(){'use strict',
       $(this).addClass('active');
       var selector = $(this).attr('data-filter');
       $portfolio.isotope({ filter: selector });
-      return false;
     });
   }
-});
-//Pretty Photo
-$("a[data-gallery^='prettyPhoto']").prettyPhoto({
-  social_tools: false
-});
+
+})
+
